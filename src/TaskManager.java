dce9a410b325dc4f62014876a444daf48522ada0
vlolad <vlad19.99@yandex.ru>
2022-04-30 14:16:52 +0300

    Refactor methods

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class TaskManager {
@@ -5,212 +6,199 @@ public class TaskManager {
     private Integer id = 0;
 
     // Мапы для хранения задач
-    HashMap<Integer, Task> tasksMap = new HashMap<>();
-    HashMap<Integer, EpicTask> epicMap = new HashMap<>();
-    HashMap<Integer, SubTask> subTaskMap = new HashMap<>();
+    private final HashMap<Integer, Task> tasks = new HashMap<>();
+    private final HashMap<Integer, EpicTask> epics = new HashMap<>();
+    private final HashMap<Integer, SubTask> subTasks = new HashMap<>();
 
-    // Метод для вывода необходимого вида комманд
-    public void getAllTasks(Commands command){
-        switch (command){
-            case ALL: {
-                for (Task task : tasksMap.values()){
-                    System.out.println(task);
-                }
-                for (int epicID : epicMap.keySet()){ // Удобнее выводить сразу сортированные по эпикам сабтаски
-                    getEpicSubTasks(epicID);
-                }
-                break;
-            }
-            case TASK: {
-                for (Task task : tasksMap.values()){
-                    System.out.println(task);
-                }
-                break;
-            }
-            case EPIC: {
-                for (EpicTask epic : epicMap.values()){
-                    System.out.println(epic);
-                }
-                break;
-            }
-            case SUBTASK: {
-                for (SubTask subTask : subTaskMap.values()){
-                    System.out.println(subTask);
-                }
-                break;
-            }
-            default:{
-                System.out.println("Command not exist.");
-                break;
-            }
+    // Методы для вывода необходимого вида тасков в виде списка
+    public ArrayList<Task> getTasks(){
+        return new ArrayList<>(tasks.values());
+    }
+
+    public ArrayList<EpicTask> getEpics(){
+        return new ArrayList<>(epics.values());
+    }
+
+    public ArrayList<SubTask> getSubTasks(){
+        return new ArrayList<>(subTasks.values());
+    }
+
+    // Методы для удаления определенных типов таск
+    public void clearTasks(){
+        tasks.clear();
+    }
+
+    public void clearEpics(){
+        epics.clear();
+        subTasks.clear();
+    }
+
+    public void clearSubTasks(){
+        subTasks.clear();
+        for (EpicTask epic : epics.values()){ // Также очищаются списки подзадач в эпиках и идет проверка их статуса
+            epic.clearSubTasksIDs();
+            checkEpicForDone(epic.getId());
         }
     }
 
-    // Метод для удаления определенных типов комманд
-    public void deleteAllTasks (Commands command){
-        switch (command){
-            case ALL: {
-                tasksMap.clear();
-                epicMap.clear();
-                subTaskMap.clear();
-                break;
-            }
-            case TASK: {
-                tasksMap.clear();
-                break;
-            }
-            case EPIC: { // Удаляю также и Сабтаски
-                epicMap.clear();
-                subTaskMap.clear();
-                break;
-            }
-            case SUBTASK: {
-                subTaskMap.clear();
-                break;
-            }
-            default:{
-                System.out.println("Command not exist.");
-                break;
-            }
+    // Методы для получения конкретного вида таски по ID
+    public Task getTaskByID(int id){
+        if (tasks.containsKey(id)){
+            return tasks.get(id);
+        } else {
+            System.out.println("Task not found.");
+            return null;
         }
     }
 
-    // Получение таска по ID
-    public void getTaskByID(int id){
-        if (tasksMap.containsKey(id)){
-            System.out.println(tasksMap.get(id));
-        } else if (epicMap.containsKey(id)){
-            System.out.println(epicMap.get(id));
-        } else if (subTaskMap.containsKey(id)){
-            System.out.println(subTaskMap.get(id));
+    public Task getEpicByID(int id){
+        if (epics.containsKey(id)){
+            return epics.get(id);
+        } else {
+            System.out.println("Epic not found.");
+            return null;
+        }
+    }
+
+    public Task getSubTaskByID(int id){
+        if (subTasks.containsKey(id)){
+            return subTasks.get(id);
+        } else {
+            System.out.println("Subtask not found.");
+            return null;
+        }
+    }
+
+    // Методы для создания тасков
+
+    public void createTask(Task task){
+        tasks.put(generateID(), task);
+    }
+
+    public void createEpic(EpicTask epic){
+        epics.put(generateID(), epic);
+    }
+
+    public void createSubTask(SubTask subTask){
+        if (epics.containsKey(subTask.getEpicID())){
+            int id = generateID();
+            subTasks.put(id, subTask);
+            epics.get(subTask.getEpicID()).addSubTaskID(id);
+            checkEpicForDone(subTask.getEpicID());
+        } else {
+            System.out.println("There is no epic with ID " + subTask.getEpicID());
+        }
+    }
+
+    // Методы для удаления тасков по ID
+    public void deleteTaskByID(int id){
+        if (tasks.containsKey(id)){
+            tasks.remove(id);
         } else {
             System.out.println("Task not exist.");
         }
     }
 
-    // Метод для создания тасков и эпиков
-    public void createTask(Commands command, Task task){
-        int id = generateID();
-        task.setId(id); // Присваиваем ID новой задаче
-        switch (command){
-            case TASK: {
-                tasksMap.put(id, task);
-                break;
+    public void deleteEpicByID(int id){
+        if (epics.containsKey(id)){
+            for (Integer subTaskID : epics.get(id).getSubTasksIDs()){
+                subTasks.remove(subTaskID);
             }
-            case EPIC: {
-                EpicTask epic = (EpicTask) task;
-                epicMap.put(id, epic);
-                break;
-            }
-            case SUBTASK: {
-                SubTask subTask = (SubTask) task;
-                subTaskMap.put(id, subTask);
-                epicMap.get(subTask.getEpicID()).addSubTaskID(id);
-                break;
-            }
-            default: {
-                System.out.println("Command not exist.");
-                break;
-            }
-        }
-    }
-
-    // Метод удаления задачи по ID
-    public void deleteByID(int id){
-        if (tasksMap.containsKey(id)){
-            tasksMap.remove(id);
-            System.out.println("Task (ID " + id + ") successfully removed.");
-        } else if (epicMap.containsKey(id)){
-            // Сначала удаляются все связанные подзадачи, а после - эпик.
-            for (Integer subTaskID : epicMap.get(id).getSubTasksIDs()){
-                subTaskMap.remove(subTaskID);
-            }
-            epicMap.remove(id);
-            System.out.println("Epic (ID " + id + ") successfully removed.");
-        } else if (subTaskMap.containsKey(id)){
-            subTaskMap.remove(id);
-            System.out.println("Subtask (ID " + id + ") successfully removed.");
+            epics.remove(id);
         } else {
-            System.out.println("Task not exist.");
+            System.out.println("Epic not exist.");
         }
     }
 
-    //Метод для обновления существующей задачи
-    public void updateTask(int id, Task newTask){
-        newTask.setId(id);
-        if (newTask instanceof EpicTask && epicMap.containsKey(id)){
-            EpicTask newEpic = (EpicTask) newTask;
-            newEpic.setSubTasksIDs(epicMap.get(id).getSubTasksIDs());
-            epicMap.put(id, newEpic);
-            System.out.println("Update Epic under ID: " + id);
-        } else if (newTask instanceof SubTask && subTaskMap.containsKey(id)) {
-            SubTask newSubTask = (SubTask) newTask;
-            subTaskMap.put(id, newSubTask);
-            System.out.println("Update SubTask under ID: " + id);
+    public void deleteSubTaskByID(int id){
+        if (subTasks.containsKey(id)){
+            epics.get(subTasks.get(id).getEpicID()).removeSubTaskID(id); // Удаляем ID сабтаски из эпика
+            checkEpicForDone(subTasks.get(id).getEpicID()); // Обновляем статус
+            subTasks.remove(id); // Наконец удаляем сабтаск
+        } else {
+            System.out.println("Subtask not exist.");
+        }
+    }
+
+    // Методы для обновления существующих тасков
+    public void updateTask(Task newTask){
+        if (tasks.containsKey(newTask.getId())){
+            tasks.put(newTask.getId(), newTask);
+        } else {
+            System.out.println("Task with this ID is not exist.");
+        }
+    }
+
+    public void updateEpic(EpicTask newEpic){
+        if (epics.containsKey(newEpic.getId())){
+            // Передаем обновленному эпику список старых сабтасков
+            newEpic.setSubTasksIDs(epics.get(newEpic.getId()).getSubTasksIDs());
+            epics.put(newEpic.getId(), newEpic);
+        } else {
+            System.out.println("Epic with this ID is not exist.");
+        }
+    }
+
+    public void updateSubTask(SubTask newSubTask){
+        if (subTasks.containsKey(newSubTask.getId()) && epics.containsKey(newSubTask.getEpicID())){
+            subTasks.put(newSubTask.getId(), newSubTask);
             checkEpicForDone(newSubTask.getEpicID());
-        } else if (tasksMap.containsKey(id)) {
-            tasksMap.put(id, newTask);
-            System.out.println("Update Task under ID: " + id);
         } else {
-            System.out.println("Error. ID not found.");
+            System.out.println("Subtask or Epic with this ID is not exist.");
         }
     }
 
-    /* Метод для изменения статусов задач.
-    Также можно обновлять статус и через метод updateTask
-     */
-    public void updateStatus(int id, TaskStatus status){
-        if (tasksMap.containsKey(id)){
-            tasksMap.get(id).setStatus(status);
-        } else if (epicMap.containsKey(id)){
-            System.out.println("Cannot change epic status manually.");
-        } else if (subTaskMap.containsKey(id)){
-            subTaskMap.get(id).setStatus(status);
-            checkEpicForDone(subTaskMap.get(id).getEpicID());// Одновременно проверяет, меняется ли статус эпика
-        } else {
-            System.out.println("Task not exist.");
-        }
-    }
-
-    // Метод для получения всех сабтасков одного эпика
+    // Метод для получения списка всех сабтасков одного эпика
     public void getEpicSubTasks(int epicID){
-        if (epicMap.containsKey(epicID)){
-            System.out.println(epicMap.get(epicID)); //Показываем Эпик
-            for (Integer subID : epicMap.get(epicID).getSubTasksIDs()){
-                System.out.println(subTaskMap.get(subID));
+        if (epics.containsKey(epicID)){
+            System.out.println(epics.get(epicID)); //Показываем Эпик
+            for (Integer subID : epics.get(epicID).getSubTasksIDs()){
+                System.out.println(subTasks.get(subID));
             }
         } else {
             System.out.println("Cannot find EpicTask by this ID.");
         }
     }
 
+    /* Уточню по работе метода checkEpicForDone(), пока не хочу полностью править его логику
+    * Цикл for в методе один, чтобы проверить все сабтаски внутри
+    * Сначала идет проверка на то, есть ли в Эпике сатбаски со статусом не-NEW. Если есть - меняем checkNotAllNEW
+    * Далее считаем, сколько из сабтасков имеют статус DONE используя счетчик
+    * После чего смотрим по имеющимся сведениям, что делать:
+    * 1. Если все сабтаски NEW (!checkNotAllNEW) или в эпике нет сабтасков - то он NEW
+    * 2. Если число сабтасков, которые DONE, равняется общему числу сабтасков, то статус эпика DONE
+    * 3. В остальных случаях статус эпика IN_PROGRESS
+    *
+    * Если всё же есть реализация лучше - возвращайте после ревью, буду пыхтеть дальше)
+    * P.S. Переменные были названы криво и было кучу лишних строк, всё поправил*/
+
     // Проверка стстуса Эпика
     private void checkEpicForDone(int epicID){
-        TaskStatus newStatus;
-        boolean checkNEW = false;
-        boolean checkAllDONE = false;
-        int subTasksLength = epicMap.get(epicID).getSubTasksIDs().size();
-        int countDONE = 0; // IDEA ругалась без инициализации
-        for (Integer id : epicMap.get(epicID).getSubTasksIDs()){
+        boolean checkNotAllNEW = false;
+        int subTasksLength = epics.get(epicID).getSubTasksIDs().size();
+        int countDONE = 0; // Понял!
+        for (Integer id : epics.get(epicID).getSubTasksIDs()){
             // Проверка, все ли NEW
-            if (subTaskMap.get(id).getStatus() != TaskStatus.NEW){
-                checkNEW = true;
+            if (subTasks.get(id).getStatus() == TaskStatus.IN_PROGRESS){
+                epics.get(epicID).setStatus(TaskStatus.IN_PROGRESS);
+                return;
+            }
+            if (subTasks.get(id).getStatus() != TaskStatus.NEW){
+                checkNotAllNEW = true;
             }
             // Проверка, сколько задач имеют статус DONE
-            if (subTaskMap.get(id).getStatus() == TaskStatus.DONE){
+            if (subTasks.get(id).getStatus() == TaskStatus.DONE){
                 countDONE++;
             }
         }
-        if (subTasksLength == countDONE) checkAllDONE = true; // Обновление переменной, если все DONE
         // Выбираем новый статус и присваиваем
-        if (!checkNEW || subTasksLength == 0){
-            newStatus = TaskStatus.NEW;
-        } else if (checkAllDONE) {
-            newStatus = TaskStatus.DONE;
+        if (!checkNotAllNEW || subTasksLength == 0){
+            epics.get(epicID).setStatus(TaskStatus.NEW);
+        } else if (subTasksLength == countDONE) {
+            epics.get(epicID).setStatus(TaskStatus.DONE);
         } else {
-            newStatus = TaskStatus.IN_PROGRESS;
+            epics.get(epicID).setStatus(TaskStatus.IN_PROGRESS);
         }
-        epicMap.get(epicID).setStatus(newStatus);
     }
 
     // Метод генерирует ID для задач. Хотел сделать через UUID, но, кажется, пока это избыточно
