6fede1f765d9b25199f666342a89d5cdaacae2c6
vlolad <vlad19.99@yandex.ru>
2022-04-30 14:15:19 +0300

    Add new tests

--- src/Main.java
+++ src/Main.java
@@ -6,32 +6,34 @@ public class Main {
         System.out.println("Поехали!");
 
         TaskManager taskManager = new TaskManager();
-        // Тестовое создание тасков
-        taskManager.createTask(Commands.TASK, new Task("Task1", "to be or not")); // id 1
-        taskManager.createTask(Commands.TASK, new Task("Task2", "not be to or")); // id 2
-        taskManager.createTask(Commands.EPIC, new EpicTask("FirstEpic", "boom")); // id 3
-        taskManager.createTask(Commands.EPIC, new EpicTask("SecondEpic", "why")); // id 4
-        taskManager.createTask(Commands.SUBTASK, new SubTask("1subtask1", "hehah", 3)); // id 5
-        taskManager.createTask(Commands.SUBTASK, new SubTask("1subtask2", "description??", 3)); // id 6
-        taskManager.createTask(Commands.SUBTASK, new SubTask("LonelyTask", "hohoh", 4)); // id 7
-        //Печатаем все задачи
-        taskManager.getAllTasks(Commands.ALL);
+        // Создайте 2 задачи, один эпик с 2 подзадачами, а другой эпик с 1 подзадачей.
+        taskManager.createTask(new Task("Task1", "hehe")); // id 1
+        taskManager.createTask(new Task("Task2", "lol")); // id 2
+        taskManager.createEpic(new EpicTask("FirstEpic", "boom")); // id 3
+        taskManager.createSubTask(new SubTask("1subtask1", "hehah", 3)); // id 4
+        taskManager.createSubTask(new SubTask("1subtask2", "description??", 3)); // id 5
+        taskManager.createEpic(new EpicTask("NotFirstEpic", "null")); // id 6
+        taskManager.createSubTask(new SubTask("secondEpicSubtask", "description??", 6)); // id 7
+        // Распечатайте списки эпиков, задач и подзадач
+        System.out.println(taskManager.getEpics());
+        System.out.println(taskManager.getTasks());
+        System.out.println(taskManager.getSubTasks());
         System.out.println("");
-        //Меняем статусы
-        taskManager.updateStatus(1, TaskStatus.IN_PROGRESS);
-        taskManager.updateStatus(2, TaskStatus.DONE);
-        taskManager.updateStatus(5, TaskStatus.IN_PROGRESS);
-        taskManager.updateStatus(6, TaskStatus.DONE);
-        taskManager.updateStatus(7, TaskStatus.DONE);
-        taskManager.updateStatus(3, TaskStatus.IN_PROGRESS); // Выскочит сообщение, что так нельзя
-        //Печатаем все задачи
-        taskManager.getAllTasks(Commands.ALL);
+        // Измените статусы созданных объектов, распечатайте.
+        taskManager.updateTask(new Task(1, "UpdatedTask", "дай бы бог завелось", TaskStatus.DONE));
+        taskManager.updateSubTask(new SubTask(5, "1subtask2 v. 2", "zhivem",
+                TaskStatus.IN_PROGRESS, 3));
+        taskManager.updateSubTask(new SubTask(7, "secondEpicEnds", "nice", TaskStatus.DONE, 6));
+        System.out.println(taskManager.getEpics());
+        System.out.println(taskManager.getTasks());
+        System.out.println(taskManager.getSubTasks());
         System.out.println("");
-        // Удаляем одну задачу и один эпик
-        taskManager.deleteByID(1);
-        taskManager.deleteByID(4);
-        //Печатаем все задачи. Снова
-        taskManager.getAllTasks(Commands.ALL);
+        // Попробуйте удалить одну из задач и один из эпиков
+        taskManager.deleteTaskByID(1);
+        taskManager.deleteEpicByID(3);
+        System.out.println(taskManager.getEpics());
+        System.out.println(taskManager.getTasks());
+        System.out.println(taskManager.getSubTasks());
     }
 }
 
