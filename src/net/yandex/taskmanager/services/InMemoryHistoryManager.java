18b6098d9d9a65eeb44beff092102257fa27840b
vlolad <vlad19.99@yandex.ru>
2022-06-28 14:03:30 +0300

    Beautify code and add check for null in method add(Task)

--- src/net/yandex/taskmanager/services/InMemoryHistoryManager.java
+++ src/net/yandex/taskmanager/services/InMemoryHistoryManager.java
@@ -10,30 +10,34 @@ import java.util.Map;
 public class InMemoryHistoryManager implements HistoryManager {
 
     //private final List<Task> history = new ArrayList<>();
-    private final Map <Integer, Node> customLinkedList = new HashMap<>();
+    private final Map<Integer, Node> customLinkedList = new HashMap<>();
     private Node first;
     private Node last;
 
     @Override
-    public void add(Task task){
+    public void add(Task task) {
         /*if (last != null && task.equals(last.body)){ // Если переданный объект уже последний в истории, метод отключается.
             return;
         } else {*/
-            if (customLinkedList.containsKey(task.getId())){
-                removeNode(customLinkedList.get(task.getId()));
-            }
-            linkLast(task);
-            customLinkedList.put(task.getId(), last);
+        if (task == null){
+            return;
+        }
+
+        if (customLinkedList.containsKey(task.getId())) {
+            removeNode(customLinkedList.get(task.getId()));
+        }
+        linkLast(task);
+        customLinkedList.put(task.getId(), last);
         //}
     }
 
-    private void linkLast(Task task){
-        if (customLinkedList.isEmpty()){
+    private void linkLast(Task task) {
+        if (customLinkedList.isEmpty()) {
             first = new Node(null, task, null);
             last = first;
         } else {
             Node secondLast = last;
-            last = new Node (secondLast, task, null);
+            last = new Node(secondLast, task, null);
             secondLast.next = last;
         }
     }
@@ -59,23 +63,23 @@ public class InMemoryHistoryManager implements HistoryManager {
         node.body = null;
     }
 
-    private List<Task> getTasks(){
+    private List<Task> getTasks() {
         List<Task> taskList = new ArrayList<>();
-        for (Node a = first; a != null; a = a.next){
+        for (Node a = first; a != null; a = a.next) {
             taskList.add(a.body);
         }
         return taskList;
     }
 
     @Override
-    public void remove(int id){ // переписал метод с учетом нового функционала
-        if (customLinkedList.containsKey(id)){
+    public void remove(int id) { // переписал метод с учетом нового функционала
+        if (customLinkedList.containsKey(id)) {
             removeNode(customLinkedList.get(id));
         }
     }
 
     @Override
-    public List<Task> getHistory(){
+    public List<Task> getHistory() {
         return getTasks();
     }
 
@@ -84,7 +88,7 @@ public class InMemoryHistoryManager implements HistoryManager {
         private Task body;
         private Node next;
 
-        public Node(Node prev, Task body, Node next){
+        public Node(Node prev, Task body, Node next) {
             this.body = body;
             this.prev = prev;
             this.next = next;
