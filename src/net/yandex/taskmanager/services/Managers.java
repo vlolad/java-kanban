ffda16f70729f674ab443f132edd1341e2a00874
vlolad <vlad19.99@yandex.ru>
2022-07-21 00:43:05 +0300

    Add HTTPTaskManager

--- src/net/yandex/taskmanager/services/Managers.java
+++ src/net/yandex/taskmanager/services/Managers.java
@@ -1,6 +1,8 @@
 package net.yandex.taskmanager.services;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 public class Managers {
 
@@ -14,5 +16,9 @@ public class Managers {
 
     public static FileBackedTasksManager getFileBacked(String csvFileName){
         return new FileBackedTasksManager(new File(csvFileName + ".csv"));
+    }
+
+    public static HTTPTaskManager getHTTPManager(URL address) throws IOException, InterruptedException {
+        return new HTTPTaskManager(address);
     }
 }
