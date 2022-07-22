0a76f20e5bb9c310912c96e69c2c4df50d04961b
vlolad <vlad19.99@yandex.ru>
2022-07-23 01:59:38 +0300

    Fix throws in methods

--- src/net/yandex/taskmanager/services/KVTaskClient.java
+++ src/net/yandex/taskmanager/services/KVTaskClient.java
@@ -10,34 +10,75 @@ import java.net.http.HttpResponse;
 public class KVTaskClient {
 
     private final HttpClient client;
-    private final String API_TOKEN;
-    private final URL serverURL;
+    private String API_TOKEN;
+    private URL serverURL;
 
-    public KVTaskClient(URL address) throws IOException, InterruptedException {
+    public KVTaskClient(String address) {
         this.client = HttpClient.newHttpClient(); //Тут this не нужно, но кажется, что так читабельнее
-        this.serverURL = address;
-        URI url = URI.create(address.toString() + "/register");
-        HttpRequest request = HttpRequest.newBuilder().uri(url).GET().build();
-        HttpResponse<String> token = client.send(request, HttpResponse.BodyHandlers.ofString());
-        this.API_TOKEN = token.body();
+        register(address);
     }
 
     public String getAPI_TOKEN() {
         return API_TOKEN;
     }
 
-    public void put(String key, String json) throws IOException, InterruptedException {
+    public void put(String key, String json) {
         URI url = URI.create(serverURL + "/save/" + key + "?API_TOKEN=" + API_TOKEN);
         final HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString(json);
         HttpRequest request = HttpRequest.newBuilder().uri(url).POST(body).build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        try {
+            HttpResponse<Void> response = client.send(request, HttpResponse.BodyHandlers.discarding());
+
+            if (response.statusCode() != 200) {
+                throw new TaskClientException("Запись на сервер не удалась, код " + response.statusCode());
+            }
+
+        } catch (IOException | InterruptedException e) {
+            throw new TaskClientException("Ошибка при попытке записать данные на сервер.");
+        }
+
     }
 
-    public String load(String key) throws IOException, InterruptedException {
+    public String load(String key) {
         URI url = URI.create(serverURL + "/load/" + key + "?API_TOKEN=" + API_TOKEN);
         HttpRequest request = HttpRequest.newBuilder().uri(url).GET().build();
-        HttpResponse<String> value = client.send(request, HttpResponse.BodyHandlers.ofString());
-        return value.body();
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() != 200) {
+                throw new TaskClientException("Загрузка данных с сервера не удалась, код " + response.statusCode());
+            }
+
+            return response.body();
+
+        } catch (IOException | InterruptedException e) {
+            throw new TaskClientException("Ошибка при попытке загрузить данные с сервера.");
+        }
     }
 
+    private void register(String address) {
+        try {
+            this.serverURL = new URL(address);
+            URI url = URI.create(address + "/register");
+            HttpRequest request = HttpRequest.newBuilder().uri(url).GET().build();
+            HttpResponse<String> token =
+                    client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (token.statusCode() != 200) {
+                throw new TaskClientException("Регистрация менеджера не удалась, сервер отправил код "
+                        + token.statusCode());
+            }
+
+            this.API_TOKEN = token.body();
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class TaskClientException extends RuntimeException {
+        public TaskClientException(String message) {
+            super(message);
+        }
+    }
 }
